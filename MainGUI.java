e8a7dd1011e5c3ec8e0c1ec68d9e3a0ae76e63fe
Judyyy <55216250+zl2877@users.noreply.github.com>
2022-08-05 11:26:10 -0400

    Create MainGUI.java

---
+++ MainGUI.java
@@ -0,0 +1,62 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnAdd;
+	private JButton btnSearch;
+	private JButton btnShow;
+	private JButton btnExit;
+	@SuppressWarnings("deprecation")
+	public void btnAdd_click() {
+		AddGUI addgui=new AddGUI();
+		addgui.show();
+	}
+	@SuppressWarnings("deprecation")
+	public void btnShowAll_click() {
+		ShowAllGUI showallgui= new ShowAllGUI();
+		showallgui.show_contacts();
+		showallgui.show();
+	}
+	public MainGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnAdd = new JButton("Start Order");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnAdd_click();
+			}
+		});
+		btnAdd.setBounds(153, 38, 117, 29);
+		contentPane.add(btnAdd);
+		
+		
+		btnShow = new JButton("View Cart");
+		btnShow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnShowAll_click();
+			}
+		});
+		btnShow.setBounds(153, 114, 117, 29);
+		contentPane.add(btnShow);
+		
+
+	}
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+}
