6eff93bc31a9dce3d6e19a41cf6a1729e9b83428
Judyyy <55216250+zl2877@users.noreply.github.com>
2022-08-08 14:51:46 -0400

    Update PriceCalculator.java

--- PriceCalculator.java
+++ PriceCalculator.java
@@ -1,4 +1,32 @@
 
 public class PriceCalculator {
+  public String topping;
+	public String size;
+	public float toppingPrice;
+	public float sizePrice;
+	public float totalPrice;
+	
 
+	float PriceCalculator(String topping, String size) {
+		
+		totalPrice = 5;
+		if (topping.equalsIgnoreCase("no topping")) {
+			totalPrice+=0;
+		}
+		else {
+			totalPrice+=1;
+		}
+		
+		if (size.equalsIgnoreCase("small")) {
+			totalPrice+=0;
+		}
+		else if (size.equalsIgnoreCase("regular")) {
+			totalPrice+=1;
+		}
+		else if (size.equalsIgnoreCase("large")) {
+			totalPrice+=2;
+		}
+		return totalPrice;
+	}
+	
 }
