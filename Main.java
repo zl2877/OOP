0632d17a74eeb14f3f7537099a9b4e2bfc7e3b0d
Judyyy <55216250+zl2877@users.noreply.github.com>
2022-08-08 12:46:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@
 public class Main {
 	public static void main(String []args) {
 		
+		MainGUI maingui=new MainGUI();
+		maingui.show();
 		
 		MainGUI firsttry = new MainGUI();
 		firsttry.show();
+		
 		
 		
 		
