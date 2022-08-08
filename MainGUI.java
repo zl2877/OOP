82fe9b062585df09c1ba26c907842a1b56b017bf
jiongyun <103552097+jiongyun@users.noreply.github.com>
2022-08-08 14:54:08 -0400

    Update MainGUI.java

--- MainGUI.java
+++ MainGUI.java
@@ -1,73 +1,61 @@
+package BubbleTea;
+
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JComboBox;
-import javax.swing.UIManager;
-import java.awt.BorderLayout;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 public class MainGUI extends JFrame {
 
-	static JFrame f;
-	static JPanel mainpanel;
-	static JButton b, b1, b2;
-	static JLabel lb1;
-	
-	/*
+	private JPanel contentPane;
+	private JButton btnAdd;
+	private JButton btnSearch;
+	private JButton btnShow;
+	private JButton btnExit;
+	@SuppressWarnings("deprecation")
 	public void btnAdd_click() {
 		AddGUI addgui=new AddGUI();
 		addgui.show();
 	}
-	
-	public void btnCart_click() {
-		CartGUI cartgui= new CartGUI();
+	@SuppressWarnings("deprecation")
+	public void btnShowAll_click() {
+		CartGUI cartgui=new CartGUI();
 		cartgui.show();
 	}
-	*/
 	public MainGUI() {
-		
-		f = new JFrame("Welcome");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		JPanel mainpanel = new JPanel();
-		mainpanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(mainpanel);
-		mainpanel.setLayout(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lb1 = new JLabel("Welcome to our boba tea shop");
-		lb1.setBounds(126, 30, 204, 29);
-
+		btnAdd = new JButton("Start Order");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnAdd_click();
+			}
+		});
+		btnAdd.setBounds(153, 38, 117, 29);
+		contentPane.add(btnAdd);
 		
-		 b = new JButton("Start Order");
-			b.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					// b();
-				}
-			});
-	     b1 = new JButton("View Cart");
-			b1.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					// b1();
-				}
-			});
-	  
-	     b.setBounds(110, 90, 239, 16);
-	     b1.setBounds(110, 140, 239, 16);
-	   
-	     
-	     mainpanel.add(b);
-	     mainpanel.add(b1);
-		mainpanel.add(lb1);
-	     //f.add(mainpanel);
-	    // f.setSize(300, 300);
-
+		
+		btnShow = new JButton("View Cart");
+		btnShow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnShowAll_click();
+			}
+		});
+		btnShow.setBounds(153, 114, 117, 29);
+		contentPane.add(btnShow);
+		
 	}
+	
+	
+	
 }
