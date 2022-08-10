82dedf7c0e1572c221d73b851d22bf7edf8db9d7
sunjy <103552097+jiongyun@users.noreply.github.com>
2022-08-10 00:27:17 -0400

    Update MainGUI.java

--- MainGUI.java
+++ MainGUI.java
@@ -9,50 +9,47 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.UIManager;
+import java.awt.Font;
+import java.awt.SystemColor;
 
 public class MainGUI extends JFrame {
 
 	private JPanel contentPane;
-	private JButton btnAdd;
-	private JButton btnSearch;
-	private JButton btnShow;
-	private JButton btnExit;
+	private JButton btnStart;
+	private JLabel lblWelcome;
+	
 	@SuppressWarnings("deprecation")
 	public void btnAdd_click() {
 		AddGUI addgui=new AddGUI();
 		addgui.show();
 	}
-	@SuppressWarnings("deprecation")
-	public void btnShowAll_click() {
-		CartGUI cartgui=new CartGUI();
-		cartgui.show();
-	}
 	public MainGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(221, 160, 221));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		btnAdd = new JButton("Start Order");
-		btnAdd.addActionListener(new ActionListener() {
+		btnStart = new JButton("Start Order");
+		btnStart.setForeground(UIManager.getColor("Button.select"));
+		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				btnAdd_click();
 			}
 		});
-		btnAdd.setBounds(153, 38, 117, 29);
-		contentPane.add(btnAdd);
+		btnStart.setBounds(161, 138, 117, 29);
+		contentPane.add(btnStart);
 		
-		
-		btnShow = new JButton("View Cart");
-		btnShow.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				btnShowAll_click();
-			}
-		});
-		btnShow.setBounds(153, 114, 117, 29);
-		contentPane.add(btnShow);
+		lblWelcome = new JLabel("Welcome to our online ordering system for bubble tea!");
+		lblWelcome.setFont(new Font("Lucida Grande", Font.ITALIC, 15));
+		lblWelcome.setForeground(Color.PINK);
+		lblWelcome.setBounds(27, 85, 417, 41);
+		contentPane.add(lblWelcome);
 		
 	}
 	
