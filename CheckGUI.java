f09e8c128c74f7dced5cf32b7bbe8f5937e68a99
sunjy <103552097+jiongyun@users.noreply.github.com>
2022-08-10 00:25:55 -0400

    Update CheckGUI.java

--- CheckGUI.java
+++ CheckGUI.java
@@ -10,10 +10,16 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
 
 public class CheckGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel lblthank;
+	private JLabel lbltotal;
+	private JLabel lblprice;
+
 
 	/**
 	 * Launch the application.
@@ -42,17 +48,27 @@ public class CheckGUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Your total is:  ");
-		lblNewLabel.setBounds(122, 122, 137, 16);
-		contentPane.add(lblNewLabel);
+		JLabel lbltotal = new JLabel("Your total is:  ");
+		lbltotal.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		lbltotal.setForeground(new Color(250, 128, 114));
+		lbltotal.setBounds(145, 122, 137, 16);
+		contentPane.add(lbltotal);
 		
-		JLabel lblNewLabel_1 = new JLabel("Thank you for your purchase");
-		lblNewLabel_1.setBounds(139, 82, 181, 16);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblthank = new JLabel("Thank you for your purchase...");
+		lblthank.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 15));
+		lblthank.setForeground(new Color(255, 182, 193));
+		lblthank.setBounds(83, 83, 277, 16);
+		contentPane.add(lblthank);
 		
-		JLabel lbl_price = new JLabel(" ");
-		lbl_price.setBounds(210, 122, 61, 16);
-		contentPane.add(lbl_price);
+		JLabel lblprice = new JLabel(" ");
+		lblprice.setForeground(new Color(250, 128, 114));
+		lblprice.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblprice.setBounds(246, 122, 61, 16);
+		
+		
+		contentPane.add(lblprice);
+		AddGUI add=new AddGUI();
+		lblprice.setText(String.valueOf(add.total));
 	}
 
 }
