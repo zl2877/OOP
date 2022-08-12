4ce57c3bd21c9aa539989e8fd76d58927b9dd62d
Judyyy <55216250+zl2877@users.noreply.github.com>
2022-08-11 23:27:26 -0400

    Update Tea.java

--- BubbleTea/Tea.java
+++ BubbleTea/Tea.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Tea {
 	
-    private String teaType;
+        private String teaType;
 	private String topping;
 	private String sugar;
 	private String ice;
