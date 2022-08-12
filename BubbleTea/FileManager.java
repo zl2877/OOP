a0bbf1133eb16440180ec5dfa82306466c96a312
sunjy <103552097+jiongyun@users.noreply.github.com>
2022-08-11 23:19:27 -0400

    Update FileManager.java

--- BubbleTea/FileManager.java
+++ BubbleTea/FileManager.java
@@ -5,36 +5,34 @@ import java.util.Scanner;
 public class FileManager {
 	public static int count;
 	//public File text = new File("bubbletea.txt");
-	public void clear() {
+	
+	public static void clear() {
 		File file = new File("bubbletea.txt");
 		if(file.exists()){
 		    file.delete();
 		}
 	}
 	
-	public boolean save(Tea t) {
-		boolean result;
+	public void save(Tea t) {
+
 		try {
 			FileWriter fw=new FileWriter("bubbletea.txt",true);
 			count++;
 			fw.write("Item "+count+": ");
-			fw.write(t.getTeaType()+"\n");
+			fw.write(t.getTeaType()+"  ------  "+"Price: "+t.getPrice()+"\n");
 			fw.write("(Customize: +"+t.getTopping()+"/"+t.getSugar()+" /"+t.getIce()+" /"+t.getSize()+")"+"\n");
-			fw.write("Price: "+t.getPrice()+"\n");
 			fw.close();
-			result=true;
+
 		} catch (IOException e) {
 			e.printStackTrace();
-			result=false;
+	
 		}
-
-		return result;
 		
 	}
 	
 	public String get_all() {
 		Scanner scanner;
-		String result = "<html><font color='green'font size='1.2'>";
+		String result = "<html><font color='green'font size='small'>";
 
 		try {
 			scanner = new Scanner(new File("bubbletea.txt"));
