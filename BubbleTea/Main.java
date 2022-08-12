6d75d5ae89067e094ee73fd4effcd9c153d04810
sunjy <103552097+jiongyun@users.noreply.github.com>
2022-08-11 23:20:08 -0400

    Update Main.java

--- BubbleTea/Main.java
+++ BubbleTea/Main.java
@@ -1,13 +1,9 @@
 package BubbleTea;
 
 public class Main {
-	public static void main(String []args) {
-		
-		
+	public static void main(String []args) {	
 		MainGUI firsttry = new MainGUI();
 		firsttry.show();
-	
-		
 		
 	}
 }
